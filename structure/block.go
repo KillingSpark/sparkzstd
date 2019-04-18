cee3201e8adcb53b7b1c21a2399bce0c0130f422
Moritz Borcherding <moritz.borcherding@web.de>
2019-04-18 12:25:32 +0200

    calculate blocksize even if blocktype is illegal

--- structure/block.go
+++ structure/block.go
@@ -37,13 +37,14 @@ func (bl *Block) DecodeHeader(raw []byte) error {
 
 	bl.Header.LastBlock = (raw[0] & 0x1) == 1       //mask all but the least bit and compare
 	bl.Header.Type = BlockType((raw[0] >> 1) & 0x3) //shift LastBlock flag out and mask all other bits but the least two
-	if bl.Header.Type >= BlockTypeReserved {
-		return ErrIllegalBlockType
-	}
 
 	bl.Header.BlockSize = uint64(raw[0] >> 3) // need to shift out the three lowest bits. (type and lastblock flags)
 	bl.Header.BlockSize += uint64(raw[1]) << 5
 	bl.Header.BlockSize += uint64(raw[2]) << 13
+
+	if bl.Header.Type >= BlockTypeReserved {
+		return ErrIllegalBlockType
+	}
 
 	//maximum 128KB
 	if bl.Header.BlockSize > (128 * 1024) {
